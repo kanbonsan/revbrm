ac75a110b6857973cbc2c47be8b505bbb4d3c19a
Shuichi Tanaka <shu.at.kantaro.org@gmail.com>
2020-05-25 08:39:59 +0900

    色々開発始めてます

---
+++ fuel/app/classes/myclass.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of myclass
+ *
+ * @author dell
+ */
+class MyClass
+{
+	public static function hello(){
+		return "hello, my class.";
+	}
+}
